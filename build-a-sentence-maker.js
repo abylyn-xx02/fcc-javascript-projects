cc96dd346d2695384ae0697747cc602e72a8b10b
abylyn-xx02 <abylynrymz@gmail.com>
2026-02-20 19:27:05 +0530

    Add first two freeCodeCamp Js labs
    - Build-a-sentence-maker.js: Complete Sentence Maker Lab with variables, concatenation and console logs.
    - labs-javascript-trivia-bot.js: complete the travia bot exercise.
    
    First upload to this repo1

---
+++ build-a-sentence-maker.js
@@ -0,0 +1,44 @@
+** start of script.js **
+
+let adjective;
+let noun;
+let verb;
+let place;
+let adjective2;
+let noun2;
+
+//assigning words
+
+adjective = "sneaky";
+noun = "fox";
+verb = "danced";
+place = "forest";
+adjective2 = "golden";
+noun2 = "stars";
+
+//first story 
+
+let firstStory;
+firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " +  place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+
+//reassign the variable
+
+adjective = "brave";
+noun = "knight";
+verb = "fought";
+place = "castle";
+adjective2 = "shiny";
+noun2 = "dragons";
+
+//second story
+
+let secondStory;
+secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + "." + " The " + noun + " lived in a " +  place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+// Log both Stories
+console.log("First story: " +  firstStory);
+console.log("Second story: " + secondStory);
+
+** end of script.js **
+
---
+++ lab-javascript-trivia-bot.js
@@ -0,0 +1,34 @@
+** start of script.js **
+
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName;
+botName = "Wall-e";
+
+var botLocation;
+botLocation = "Youtopia";
+
+var favoriteLanguage;
+favoriteLanguage = "Javascript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact;
+codingFact = favoriteLanguage + " is mostly used for script in HTML and CSS" + ".";
+
+console.log(codingFact);
+
+codingFact = "While " + favoriteLanguage + " is mostly known for its usage for script. It is also used a curcial part for webdevelopment" + ".";
+
+console.log(codingFact);
+
+codingFact = "So, " + favoriteLanguage + " is best language used for scripting webdevelopment" + ".";
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! " + "- " + botName + " from " + botLocation + ".");
+
+
+** end of script.js **
+
